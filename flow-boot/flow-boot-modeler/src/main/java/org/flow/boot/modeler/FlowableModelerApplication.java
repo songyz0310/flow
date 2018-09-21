34fc7be5cf51e8f7e7511444c0b15fae7f83f1d4
宋印赠 <songyinzeng@gmail.com>
2018-09-21 11:12:14 +0800

    提交代码

--- flow-boot/flow-boot-modeler/src/main/java/org/flow/boot/modeler/FlowableModelerApplication.java
+++ flow-boot/flow-boot-modeler/src/main/java/org/flow/boot/modeler/FlowableModelerApplication.java
@@ -1,22 +1,19 @@
 package org.flow.boot.modeler;
 
-import org.flow.boot.modeler.config.MyAppDispatcherServletConfiguration;
-import org.flow.boot.modeler.config.MyApplicationConfiguration;
+import org.flowable.ui.modeler.conf.ApplicationConfiguration;
+import org.flowable.ui.modeler.servlet.AppDispatcherServletConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
-@Import({ MyApplicationConfiguration.class, MyAppDispatcherServletConfiguration.class })
+@Import({ ApplicationConfiguration.class, AppDispatcherServletConfiguration.class })
 @SpringBootApplication
 public class FlowableModelerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FlowableModelerApplication.class, args);
-        
-//        ModelsResource
-        
-//        http://localhost:8888/flowable-modeler/app/rest/models/bd66eac8-b669-11e8-a139-68ecc557e441/editor/json
-        
+
     }
 }
