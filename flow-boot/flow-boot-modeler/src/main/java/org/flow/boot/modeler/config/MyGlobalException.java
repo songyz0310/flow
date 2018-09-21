33f2b8437af98bea3d9acb1582f7eb074191b429
宋印赠 <songyinzeng@gmail.com>
2018-09-21 14:17:58 +0800

    提交异常测试

--- flow-boot/flow-boot-modeler/src/main/java/org/flow/boot/modeler/config/MyGlobalException.java
+++ flow-boot/flow-boot-modeler/src/main/java/org/flow/boot/modeler/config/MyGlobalException.java
@@ -19,15 +19,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
-public class WebGlobalException {
+public class MyGlobalException {
 
-    private static final Logger log = LoggerFactory.getLogger(WebGlobalException.class);
+    private static final Logger logger = LoggerFactory.getLogger(MyGlobalException.class);
 
     // 500异常
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(RuntimeException.class)
     public Response<?> handleRuntimeException(RuntimeException e) {
-        log.error("RuntimeException:{}", e);
+        logger.error("RuntimeException:{}", e);
         return Response.errorResponse(ErrorCode.UNKNOWN);
 
     }
@@ -36,11 +36,11 @@ public class WebGlobalException {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public Response<?> handleBindException(BindException e) {
-        log.error("参数绑定失败", e);
+        logger.error("参数绑定失败", e);
         BindingResult bindingResult = e.getBindingResult();
         FieldError error = bindingResult.getFieldError();
         String field = error.getField();
-        log.error(field);
+        logger.error(field);
         String code = error.getDefaultMessage();
         Response<?> result = Response.errorResponse(ErrorCode.HTTP_400_BIND);
         result.appendMessage(code);
@@ -51,7 +51,7 @@ public class WebGlobalException {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException.class)
     public Response<?> handleBindException(ConstraintViolationException e) {
-        log.error("参数绑定失败", e);
+        logger.error("参数绑定失败", e);
         Response<?> result = Response.errorResponse(ErrorCode.HTTP_400_VALIDATION);
         result.appendMessage(e.getMessage());
         return result;
@@ -61,7 +61,7 @@ public class WebGlobalException {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     public Response<?> handleBindException(IllegalArgumentException e) {
-        log.error("参数绑定失败", e);
+        logger.error("参数绑定失败", e);
         Response<?> result = Response.errorResponse(ErrorCode.HTTP_400_VALIDATION);
         result.appendMessage(e.getMessage());
         return result;
@@ -71,7 +71,7 @@ public class WebGlobalException {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(ServletException.class)
     public Response<?> handle403Exception(ServletException e) {
-        log.error("401异常", e);
+        logger.error("401异常", e);
         Response<?> result = Response.errorResponse(ErrorCode.HTTP_400_VALIDATION);
         result.appendMessage(e.getMessage());
         return result;
@@ -81,7 +81,7 @@ public class WebGlobalException {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
     public Response<?> handle404Exception(NoHandlerFoundException e) {
-        log.error("请求没找到异常", e);
+        logger.error("请求没找到异常", e);
         Response<?> result = Response.errorResponse(ErrorCode.HTTP_404_NOT_FOUND);
         result.appendMessage(e.getMessage());
         return result;
@@ -91,7 +91,7 @@ public class WebGlobalException {
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public Response<?> handle405Exception(HttpRequestMethodNotSupportedException e) {
-        log.error("不支持当前请求方法", e);
+        logger.error("不支持当前请求方法", e);
         Response<?> result = Response.errorResponse(ErrorCode.HTTP_405_NOT_ALLOWED);
         result.appendMessage(e.getMessage());
         return result;
@@ -101,7 +101,7 @@ public class WebGlobalException {
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public Response<?> handle415Exception(Exception e) {
-        log.error("不支持当前媒体类型", e);
+        logger.error("不支持当前媒体类型", e);
         Response<?> result = Response.errorResponse(ErrorCode.HTTP_415_MEDIA_TYPE);
         result.appendMessage(e.getMessage());
         return result;
--- flow-boot/flow-boot-modeler/src/main/java/org/flow/boot/modeler/controller/MyRemoteAccountResource.java
+++ flow-boot/flow-boot-modeler/src/main/java/org/flow/boot/modeler/controller/MyRemoteAccountResource.java
@@ -2,6 +2,7 @@ package org.flow.boot.modeler.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpSession;
 import org.flowable.ui.common.model.RemoteUser;
 import org.flowable.ui.common.model.UserRepresentation;
 import org.flowable.ui.common.security.FlowableAppUser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -25,17 +28,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/app")
 public class MyRemoteAccountResource {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyRemoteAccountResource.class);
+    private static final String SPRING_SECURITY_CONTEXT = "_SPRING_SECURITY_CONTEXT";
+    private static final String SESSION_USER = "_SESSION_USER";
+
     /**
      * GET /rest/account -> get the current user.
      */
     @GetMapping(value = "/rest/account", produces = "application/json")
-    public UserRepresentation getAccount(HttpServletRequest request) {
+    public UserRepresentation getAccount(HttpServletRequest request, HttpSession session) {
+        Object obj = session.getAttribute(SESSION_USER);
+        if (Objects.nonNull(obj)) {
+            logger.info("从缓存中获取用户");
+            return (UserRepresentation) obj;
+        }
+        logger.info("从获取用户");
+
         List<String> roles = Arrays.asList("ROLE_ADMIN", "ROLE_USER");
         UserRepresentation userRepresentation = new UserRepresentation();
         userRepresentation.setId("1");
-        userRepresentation.setFirstName("songyz");
+        userRepresentation.setFirstName("宋");
         userRepresentation.setFullName("宋印赠");
-        userRepresentation.setLastName("宋印赠");
+        userRepresentation.setLastName("印赠");
         userRepresentation.setPrivileges(roles);
 
         List<GrantedAuthority> authorities = roles.stream().map(role -> new SimpleGrantedAuthority(role))
@@ -57,10 +71,9 @@ public class MyRemoteAccountResource {
 
         // 存放authentication到SecurityContextHolder
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        HttpSession session = request.getSession(true);
         // 在session中存放security context,方便同一个session中控制用户的其他操作
-        session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
-
+        session.setAttribute(SPRING_SECURITY_CONTEXT, SecurityContextHolder.getContext());
+        session.setAttribute(SESSION_USER, userRepresentation);
         return userRepresentation;
 
     }
